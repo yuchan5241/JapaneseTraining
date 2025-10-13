fa874d90a818703488056dc2093b35c09fa87235
yuchan5241 <grigomo10@gmail.com>
2025-10-13 23:33:43 +0900

    Japanese Version 2.0

---
+++ Programs/VocaSpeak.py
@@ -0,0 +1,17 @@
+import requests
+import os
+
+# run.exe 파일 경로 "C:/Users/sunfl/AppData/Local/Programs/VOICEVOX/vv-engine/run.exe"
+os.system("C:/Users/sunfl/AppData/Local/Programs/VOICEVOX/vv-engine/run.exe")
+
+
+
+
+
+
+
+
+
+
+#voicevox를 통해서 단어 음성파일을 저장한다.
+#로컬 서버 접속은 voicevox의 run.exe 파일을 통해 연결
