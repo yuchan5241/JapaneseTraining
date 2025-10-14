b691ccd9fe8acad49c39b0feca430363a75a333f
yuchan5241 <grigomo10@gmail.com>
2025-10-15 00:03:45 +0900

    voicevox test version

--- Programs/VocaSpeak.py
+++ Programs/VocaSpeak.py
@@ -1,17 +1,53 @@
 import requests
-import os
-
-# run.exe 파일 경로 "C:/Users/sunfl/AppData/Local/Programs/VOICEVOX/vv-engine/run.exe"
-os.system("C:/Users/sunfl/AppData/Local/Programs/VOICEVOX/vv-engine/run.exe")
-
+import numpy as np
+import json
+import sounddevice as sd
 
 
+host = "127.0.0.1"
+port = "50021"
+speaker = 3
+
+def post_audio_query(text: str) -> dict:
+    params = {"text": text, "speaker": speaker}
+
+    res = requests.post(
+        f"http://{host}:{port}/audio_query",
+        params=params,
+    )
+
+    query_data = res.json()
+
+    return query_data
+
+def post_synthesis(query_data: dict) -> bytes:
+    params = {"speaker": speaker}
+    headers = {"content-type": "application/json"}
+
+    res = requests.post(
+        f"http://{host}:{port}/synthesis",
+        data = json.dumps(query_data),
+        params=params,
+        headers=headers,
+    )
+
+    return res.content
+
+def play_wavfile(wav_data: bytes):
+    sample_rate = 24000
+    wav_array = np.frombuffer(wav_data, dtype=np.int16)
+    sd.play(wav_array, sample_rate, blocking=True)
+
+def text_to_voice():
+    while True:
+        text = input("입력 : ")
+        if text == "q":
+            exit()
+        
+        res = post_audio_query(text)
+        wav = post_synthesis(res)
+        play_wavfile(wav)
 
 
-
-
-
-
-
-#voicevox를 통해서 단어 음성파일을 저장한다.
-#로컬 서버 접속은 voicevox의 run.exe 파일을 통해 연결
+if __name__ == "__main__":
+    text_to_voice()
